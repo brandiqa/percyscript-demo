4c0a620c0b04169951592158791ce84d43bd1d1f
Michael Wanyoike <micwanyoike@gmail.com>
2019-07-11 23:22:16 +0300

    Implemented Percy Snapshots

--- snapshots.js
+++ snapshots.js
@@ -3,25 +3,42 @@ const PercyScript = require('@percy/script');
 PercyScript.run(async (page, percySnapshot) => {
     // Test Home Page
     await page.goto('http://localhost:3000/');
-    await page.waitFor(2000);
+    // wait for AJAX call to complete
+    await page.waitForSelector('.loading', {
+        hidden: true
+    });
     await percySnapshot('homepage');
 
     // Test Currency Conversion
-     await page.goto('http://localhost:3000/exchange');
-     await page.waitFor(3000);
-     await page.select('select#from', 'BTC'); // Select bitcoin
-     await page.select('select#to', 'USD'); // Select US Dollar
-     await page.type('#amount', '1'); // Enter Amount
-    // Hit the convert button
-     const previousResult = await page.evaluate(() => document.querySelector('#result').innerHTML);
-     await page.click('.submit');
-     await page.waitForFunction(`document.querySelector('#result').innerHTML !== ${previousResult}`); // Wait until the result is updated
-     await percySnapshot('exchange');
+    await page.goto('http://localhost:3000/exchange');
+    // Wait for AJAX call to complete
+    await page.waitForSelector('.loading', {
+        hidden: true
+    });
+    await page.select('select#from', 'BTC'); // Select bitcoin
+    await page.select('select#to', 'USD'); // Select US Dollar
+    await page.type('#amount', '1'); // Enter Amount
+    await page.click('.submit'); // Hit the convert button
+    // wait for AJAX call to complete
+    await page.waitForSelector('.loading', {
+        hidden: true
+    });
+    await percySnapshot('exchange');
 
     // Test Historical Rates
-    //  await page.goto('http://localhost:3000/historical');
-    //  page.type('#date', '2019-07-01');
-    //  page.click('.submit'); // Click Fetch Rates Button
-    //  await page.waitForSelector('#historical-table');
-    //  await percySnapshot('historical');
+    await page.goto('http://localhost:3000/historical');
+    // wait for AJAX call to complete
+    await page.waitForSelector('.loading', {
+        hidden: true
+    });
+    // Set Calendar Input    
+    await page.evaluate(() => {
+        document.getElementById('date').value = '2019-07-01';
+    })
+    await page.click('.submit'); // Click Fetch Rates Button
+    // wait for AJAX call to complete
+    await page.waitForSelector('.loading', {
+        hidden: true
+    });
+    await percySnapshot('historical');
 });
